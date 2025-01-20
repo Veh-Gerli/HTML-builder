8b62f5a661143dd6d97f47463e0ce56186a94c39
Julia Kuzmina <veh.gerli@mail.ru>
2025-01-20 15:57:25 +0300

    01 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readFile(
-  path.join(__dirname, "text.txt"),
-  "utf-8",
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  },
-  );
+const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
+let data = "";
+readableStream.on("data", (chunk) => (data += chunk));
+readableStream.on("end", () => console.log(data));
+stream.on("error", (error) => console.log("Error", error.message));
